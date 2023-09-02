834e63ffb124f7a7fb4a4dcd88511caf0dc8f419
MINAMINO <causewaybar711@gmail.com>
2023-09-02 16:58:30 +0900

    Create evenNumber.java

---
+++ evenNumber.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 5; // 将 x 的值设置为 5
+
+        if (x % 2 == 0){ // 使用 % 运算符来判断 x 是否为偶数
+            System.out.println("x是偶数");
+        }
+        else {
+            System.out.println("x是奇数");
+        }
+    }
+}
