3817772b11462c593e44c1f0f8006e2a880c509c
MINAMINO <causewaybar711@gmail.com>
2023-09-03 22:29:24 +0900

    Create evenNumbers.java
    Even numbers within 20 printouts

---
+++ evenNumbers.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        int num = 1;
+
+        while (num < 20){
+
+            if (num % 2 == 0){
+
+                System.out.println(num);
+
+            }
+            num++;
+        }
+
+    }
+
+}
