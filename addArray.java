a959c7f9533b530bd42d4de34d0d9d40dee400f9
MINAMINO <causewaybar711@gmail.com>
2023-09-03 22:27:31 +0900

    Create addArray.java
    traverse and add up an array

---
+++ addArray.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] srgs){
+
+        int sum = 0;
+
+        int[] array = {2,5,1,78,10};
+
+        for (int x = 0; x < array.length; x++) {
+
+            sum += array[x];
+
+        }
+        System.out.println(sum);
+    }
+}
