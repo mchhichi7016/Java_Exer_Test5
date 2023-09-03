3ef0fe1ee6d449be0ce76205bd0d383e6c653829
MINAMINO <causewaybar711@gmail.com>
2023-09-03 22:34:40 +0900

    Create printSpring2.java
    Print Spring, Summer, Fall and Winter

---
+++ printSpring2.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int s = 4;
+
+        if (s == 3 || s == 4 || s == 5){
+            System.out.println("春");
+        }
+        else if (s == 6 || s == 7 || s == 8) {
+            System.out.println("夏");
+        }
+        else if (s == 9 || s == 10 || s == 11 ) {
+            System.out.println("秋");
+        }
+        else
+            System.out.println("冬");
+    }
+}
