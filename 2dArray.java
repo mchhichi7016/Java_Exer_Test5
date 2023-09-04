26438d85e9715166bc85a1a8281bf292fd7f4b22
MINAMINO <causewaybar711@gmail.com>
2023-09-04 10:36:34 +0900

    Create 2dArray.java
    traverse a 2D array

---
+++ 2dArray.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] array = { { 1, 3, 5},{4, 9, 2},{9, 4, 7}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                System.out.println(array[i][j]);
+            }
+
+        }
+    }
+}
