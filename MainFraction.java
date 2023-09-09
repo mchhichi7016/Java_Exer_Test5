a4540bb7bf2ae9b03281b069b832d076a19fe766
MINAMINO <causewaybar711@gmail.com>
2023-09-09 11:06:09 +0900

    Create MainFraction.java

---
+++ MainFraction.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Fraction f1 = new Fraction(1,3);
+        Fraction f2 = new Fraction(2,4);
+
+        System.out.println(f1.multiple(f2).toString());
+
+    }
+}
