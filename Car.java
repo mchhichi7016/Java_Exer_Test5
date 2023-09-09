2a6588c5314baeaf3de947a2922d25c8a24fdc05
MINAMINO <causewaybar711@gmail.com>
2023-09-09 09:46:17 +0900

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car extends Vehicle {
+    public class Vehicle {
+        // 默认构造函数
+    public Vehicle() {
+        }
+    }
+
+
+    private int seat;
+
+    public Car(String name, int seat) {
+        super(name);
+        this.seat = seat;
+    }
+    }
+
